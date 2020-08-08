3ef73c868cb787c6eb312d1d09df876fcaee3b8a
Chris D <dqcuong93@gmail.com>
2020-08-08 16:39:22 +0700

    run all daily applications

---
+++ run_apps.py
@@ -0,0 +1,27 @@
+import subprocess
+
+apps = (
+    'message',
+    'skype',
+    'zalo',
+    'messenger',
+    'slack',
+    'viber',
+    'telegram',
+    'whatsapp',
+    'jira',
+    'spark'
+)
+
+command = 'open -a'
+commands = ''
+
+for app in apps:
+    commands += command + ' ' + app + '; '
+
+# run() returns a CompletedProcess object if it was successful
+# errors in the created process are raised here too
+process = subprocess.run(commands, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+output = process.stdout
+
+output
