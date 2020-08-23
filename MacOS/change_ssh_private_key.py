ef21c78a76c25bab54e8d7e975562816c106d9d5
Chris D <dqcuong93@gmail.com>
2020-08-23 16:43:23 +0700

    Optimised code. Used dictionary, modified if else loop. Less code, less pain.

--- MacOS/change_ssh_private_key.py
+++ MacOS/change_ssh_private_key.py
@@ -1,29 +1,31 @@
 import subprocess
 
 def choose_ssh_file():
-    '''This function will set the default ssh private key file to "cuong@vietnam-cloud.vn"
-    Based on what user input ("cuong" or "vcs"), the program will set the correspondent ssh identity variable
-    For now, this program is harded-code with 2 accepted ssh private key: "cuong" and "vcs".
+    '''This function will set the ssh private key file automatically.
+    Based on what user input ("cuong" or "vcs"), the program will set the correspondent ssh identity variable.
+    For now, this program accepts 2 ssh private key: "cuong" and "vcs".
     '''
-    vcs_ssh_file = 'cuong@vietnam-cloud.vn'  # Default ssh file will be vcs
-    personnal_ssh_file = 'dqcuong93@gmail.com'
-    ssh_file = vcs_ssh_file
+
+    existing_ssh_file = {
+        'cuong': 'dqcuong93@gmail.com',
+        'vcs': 'cuong@vietnam-cloud.vn'
+    }
 
     while True:
-        ssh_file_name = input("Choose your ssh file name: 'cuong' or 'vcs'\n--> ")  # Prompt input
+        ssh_file_name = input(f"Choose your ssh file name {[key for key in existing_ssh_file]}:\n--> ")  # Prompt input
         ssh_file_name = ssh_file_name.strip()  # Remove all whitespace
 
-        if ssh_file_name not in ('cuong', 'vcs'):
-            print('No SSH file found!')
-            break
-        else:
-            if ssh_file_name == 'cuong': ssh_file = personnal_ssh_file
+        if ssh_file_name in existing_ssh_file:  # Check if SSH file name exist
+            ssh_file = existing_ssh_file[ssh_file_name]  # Get value of key
 
             command = f'ssh-add -D && ssh-add ~/.ssh/{ssh_file}'
 
             # run() returns a CompletedProcess object if it was successful
             # errors in the created process are raised here too
             process = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+            break
+        else:
+            print('No SSH file found! Exit!')
             break
 
 if __name__ == '__main__':
