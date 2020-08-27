c335f10d659a3ebb03f840929d0d3c34fee7e332
Chris D <dqcuong93@gmail.com>
2020-08-27 09:31:56 +0700

    Optimised code, look more elegant
    - Added default MacOS mail app.
    - Created some constants instead of variables.
    - Defined function for running applications.

--- MacOS/run_apps.py
+++ MacOS/run_apps.py
@@ -1,6 +1,8 @@
 import subprocess
 
-apps = (
+# Constant go here
+COMMAND = 'open -a '
+APPS_NAME = (
     'messages',
     'skype',
     'zalo',
@@ -11,18 +13,20 @@ apps = (
     'telegram',
     'whatsapp',
     'jira',
-    'spark'
+    'spark',
+    'mail',
 )
 
-command = 'open -a'
-commands = ''
+def run_apps():
+    list_of_commands = ''
 
-for app in apps:
-    commands += command + ' ' + app + '; '
+    # Create list of commands
+    for a in APPS_NAME:
+        list_of_commands += COMMAND + a + ';'
 
-# run() returns a CompletedProcess object if it was successful
-# errors in the created process are raised here too
-process = subprocess.run(commands, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-output = process.stdout
+    # run() returns a CompletedProcess object if it was successful
+    # errors in the created process are raised here too
+    process = subprocess.run(list_of_commands, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
 
-output
+if __name__ == '__main__':
+    run_apps()
