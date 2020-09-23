5168e67d7c93e7c36e70add1b130daf2ee392923
Chris D <dqcuong93@gmail.com>
2020-09-24 02:09:52 +0700

    Update print line

--- Python-MacOS/python-format-checking.py
+++ Python-MacOS/python-format-checking.py
@@ -15,7 +15,7 @@ def code_checked(path_to_file):
     """Python check code formatting"""
 
     list_of_commands = ""
-    print("Running:")
+    print("Checking your code!...")
     for a in APPS:
         list_of_commands += " ".join([a, path_to_file, "; "])
         print(f"~> {a + ' ' + path_to_file}")
