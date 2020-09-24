2acd45054600f83f74cc0b39ddcaf71ffa02494a
Chris D <dqcuong93@gmail.com>
2020-09-24 09:54:37 +0700

    Update services-restarting.sh

--- EDX-platform/services-restarting.sh
+++ EDX-platform/services-restarting.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# restart edx instance
+# restart edx instances
 sudo /edx/bin/supervisorctl restart lms
 sudo /edx/bin/supervisorctl restart cms
 sudo /edx/bin/supervisorctl restart edxapp_worker:
