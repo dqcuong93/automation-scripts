5bf01a4be089192c8fd85b37e7dc8529c78df186
Chris D <dqcuong93@gmail.com>
2023-02-17 23:44:36 +0700

    Improved code block and changed formatting of docstrings

--- Python-MacOS/create_web_link.py
+++ Python-MacOS/create_web_link.py
@@ -2,25 +2,30 @@
 Based on what user provided, program will create a web-link file.
 """
 
+import logging
 import os
 
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-def create_url_file():
-    """Create a web-link file
-    Create a web-link file from the specified link
+
+def create_url_file() -> None:
+    """Create a web-link file based on the user provided URL
+
+    Returns: None
+
     """
 
     current_directory_path = os.getcwd()  # Get current working directory
-    url = input("Please copy the url here:\n--> ")
+    url = input("Please copy the url here:\n--> ").strip()  # Get user provided url
 
-    file_name = input("Please input your URL file name:\n--> ")
-    file_name = file_name + ".url"  # Append '.url' to the end of file name
-    file_path = "/".join(
-        [current_directory_path, file_name]
-    )  # Join the current path and the file name together by '/'
+    # Prompt user for filename, and append '.url' to the end of the filename
+    file_name = input("Please input your URL file name:\n--> ").strip() + ".url"
+    file_path = os.path.join(current_directory_path, file_name)
 
     with open(file_path, "w", encoding="utf-8") as file:  # Create new file if not exist
         file.write(f"[InternetShortcut]\nURL={url}")
+
+    logging.info(f"\nFile '{file_name}' created successfully at '{current_directory_path}'.")
 
 
 if __name__ == "__main__":
